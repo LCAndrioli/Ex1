90f0ac82e8ba5994b99fe96e56e76804d766fa04
LCAndrioli <luiz.c.andrioli@gmail,com>
2020-02-17 16:23:45 -0300

    Aparentemente tudo esta correto, os numeros se organizam, usuario nao pode mais digitar valores incorretos, diz resultado e acertos, resultados nao se repetem e nao ficam incorretos. Letras Bugam a execucao"

--- exercicio.c
+++ exercicio.c
@@ -1,18 +1,21 @@
 #include <time.h>
 #include <stdio.h>
-#include <windows.h>
 
-int valor[6]; 
+int valor[6], resultados[6]; 
 
 void megasena();
 void pegavalor();
+void sortear();
 void comparar();
+void organizar();
 
 int main(){
 	int a;
 	
 	megasena();
     pegavalor();
+    sortear();
+    organizar();
     comparar();
     
     return 0;
@@ -39,18 +42,56 @@ void megasena(){
 
 void pegavalor(){
 	int a;
-    printf("\t\tEscolha seis numeros:\n\t\t> ");
+    printf("\t\tEscolha seis numeros acima:\n\t\t");
     for(a=0;a<6;a++){
+    	printf("numero %i: ", a+1);
     	scanf(" %i", &valor[a]);
+    	if(valor[a]<1||valor[a]>60){
+    		printf("\n\tValor incorreto. Digite novamente o ");
+    		a--;
+		}else{
+			printf("\n\t\t");
+		}
+		
 	}
 }
 
-void comparar(){
+void sortear(){
 	srand(time(NULL));
-	int a, b, resultados[6], acertos = 0;
+	int a;
 	for(a=0;a<6;a++){
 		resultados[a] = rand()%60;
 	}
+}
+
+void organizar(){
+	int a, b, m, aux;
+	for(a=0;a<5;a++){
+		m = a;
+		for(b=a+1;b<6;b++){
+			if(valor[m] > valor[b]){
+				m = b;
+			} 
+		}
+		aux = valor[a];
+		valor[a] = valor[m];
+		valor[m] = aux;
+	}
+	for(a=0;a<5;a++){
+		m = a;
+		for(b=a+1;b<6;b++){
+			if(resultados[m] > resultados[b]){
+				m = b;
+			} 
+		}
+		aux = resultados[a];
+		resultados[a] = resultados[m];
+		resultados[m] = aux;
+	}
+}
+    
+void comparar(){
+	int a, b, acertos = 0;
 	for(a=0;a<6;a++){
 		for(b=0;b<6;b++){
 			if(valor[a]==resultados[b]){
@@ -58,10 +99,23 @@ void comparar(){
 			}	
 		}
 	}
+	printf("\n\t\tNumeros jogados: ");
+	for(a=0;a<6;a++){
+		printf("%i ", valor[a]);
+	}
 	printf("\n\t\tResultado: ");
 	for(a=0;a<6;a++){
 		printf("%i ", resultados[a]);
 	}
-	printf("\n\t\tAcertou %i", acertos);
-	
+	printf("\n\t\tAcertou %i. ", acertos);
+	switch(acertos){
+		case 0:printf("");break;
+		case 1:printf("");break;
+		case 2:printf("");break;
+		case 3:printf("");break;
+		case 4:printf("Quadra");break;
+		case 5:printf("Quina");break;
+		case 6:printf("Sena");break;
+		default:printf("\n\n\t\tAlgo deu errado.\n");
+	}
 }
